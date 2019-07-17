418d4ab72db3685ca2c6fdca736eb8f939f63e4c
Marco Galluzzi <galluzzi.m@gmail.com>
2019-07-17 13:01:16 +0100

    Refactored get Purchase methods into the Purchase controller

--- src/main/java/com/linkedin/learning/reactivespring/controller/PurchaseController.java
+++ src/main/java/com/linkedin/learning/reactivespring/controller/PurchaseController.java
@@ -3,10 +3,8 @@ package com.linkedin.learning.reactivespring.controller;
 import com.linkedin.learning.reactivespring.model.Purchase;
 import com.linkedin.learning.reactivespring.service.CoinBaseService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.time.LocalDateTime;
@@ -23,4 +21,15 @@ public class PurchaseController {
         // return Mono.fromSupplier(() -> new Purchase("name", "price", LocalDateTime.now()));
         return coinBaseService.createPurchase(name);
     }
+
+    @GetMapping(value = "/{id}")
+    public Mono<Purchase> getPurchase(@PathVariable("id") String id) {
+        return coinBaseService.getPurchaseById(id);
+    }
+
+    @GetMapping(value = "/")
+    public Flux<Purchase> getAllPurchases() {
+        return coinBaseService.listaAllPurchases();
+    }
+
 }
